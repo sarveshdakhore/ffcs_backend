4a48d8b950f9cfe5d8bc9afc901675714e9d82dc
sarveshdakhore <sarveshd444@gmail.com>
2024-04-10 02:43:34 +0530

    database setup

--- database.py
+++ database.py
@@ -11,7 +11,6 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
